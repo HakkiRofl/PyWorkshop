d4792519b56dcb981793cc99a8697da4377cae2a
Ivan <103391795+HakkiRofl@users.noreply.github.com>
2022-04-11 02:29:14 +0000

    Part 2 Task 1

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-money = int(input())
-tax = 0
-if money > 15527 and money < 42708:
-  tax = 15
-elif money > 42707 and money < 132407:
-  tax = 25
-elif money > 132406:
-  tax = 28
-abc = '%.0f' % (money*tax/100)
-print(f"The tax for {money} is {tax}%. That is {abc} dollars!")
+word = input()
+if word == word[::-1]:
+  print("Yes")
+else:
+  print("No")
