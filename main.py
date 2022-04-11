5596c99ea3d9c1d47cc79642792e4bf3654a10e8
Ivan <103391795+HakkiRofl@users.noreply.github.com>
2022-04-11 00:34:23 +0000

    Task 6

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-#Task 4
-word = input()
-print(f"{word} has {len(word)} letters")
+#Which of the following examples will correctly output formatted strings?
+
+#If a code snippet raises SyntaxError or formatting doesn't actually take place in a string, 
+#consider such an option incorrect.
+"%.4f".format(3.14159265358979)
+"{1} {1} {1}".format(1, 2, 3)
+#"{1} is a {kind}".format(kind="fruit", "grapefruit") - wrong answer
+"{city} is the capital of {country}".format(country="Portugal",
+                                            city="Lisbon")
