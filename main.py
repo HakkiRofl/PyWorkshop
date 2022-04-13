2fe5659248242968d3d2c71a4af4aa39341ae25c
Ivan <103391795+HakkiRofl@users.noreply.github.com>
2022-04-13 00:23:04 +0000

    Part 5 Tasl 3 (Profit)

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
-a = input()
-b = input()
-g = ""
-for c, d in zip(a, b):
-  g += c
-  g += d
-print(g)
+mounths = ["Jan", "Feb", "Mar", "Apr", "May", "Jul", "Jun", "Aug", "Sep", "Oct", "Nov", "Dec"]
+prices = [1000, 1400, 600, 700, 5000, 4500, 1200, 7000, 8000, 10000, 1000, 2500]
+procent = "n/a "
+profit = "n/a"
+i = 0
+for mon, price in zip(mounths, prices):
+  i += 1
+  if i < len(prices):
+    print(f"{mon} {price} {procent}% {profit}")
+    procent = round((prices[i] - price) / price * 100)
+  if procent > 50:
+    profit = "great"
+  elif procent > 25 and procent <= 50:
+    profit = "decent"
+  elif procent >= 0 and procent <= 25:
+    profit = "need follow up"
+  elif procent < 0:
+    profit = "critical"
