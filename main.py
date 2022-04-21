97574568fc4d42a6b700e37a9a95941aa3df5eaa
Ivan <103391795+HakkiRofl@users.noreply.github.com>
2022-04-21 02:07:05 +0000

    Part 18 task 1

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-import math
-print(int(math.sqrt(int(input()))))
+import itertools
+
+first_names = ['Anna', 'Catarina']
+middle_names = ['Luisa', 'Maria']
+
+for first, middle in itertools.product(first_names, middle_names):
+  print(first, middle)
